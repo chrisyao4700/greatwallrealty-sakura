607cd950144a19df23b8b14313de759c514b3210
Yuan Yao <yuanyao@Yuans-MacBook-Pro-7.local>
2018-01-20 11:48:05 -0800

    FINISH ANIMATION

--- public/javascripts/index_vc.js
+++ public/javascripts/index_vc.js
@@ -1,44 +1,69 @@
-
 function toggleOnlineChatting() {
     Tawk_API.toggle();
 }
 
 
-$(function(){
+$(function () {
     //alert('I am here!');
-    $('#fh5co-primary-menu li#nav-index').addClass('active');
+    // $('#fh5co-primary-menu li#nav-index').addClass('active');
+    var page = $('#page-name').data('page');
+    switch (page) {
+        case 'purchase':
+            clickNavWithTag('nav-purchase');
+            break;
+        case 'escrow':
+            clickNavWithTag('nav-escrow');
+            break;
+        case 'loan':
+            clickNavWithTag('nav-loan');
+            break;
+        case 'design':
+            clickNavWithTag('nav-design');
+            break;
+        case 'land':
+            clickNavWithTag('nav-investment');
+            break;
+        case 'management':
+            clickNavWithTag('nav-management');
+            break;
+        case 'about':
+            break;
+        default:
+            break;
+    }
+    //alert(page);
 
 });
 
-function clickNavWithTag(tag){
+function clickNavWithTag(tag) {
     $('#fh5co-primary-menu li.active').removeClass('active');
-    $('#fh5co-primary-menu li#'+tag).addClass('active');
+    $('#fh5co-primary-menu li#' + tag).addClass('active');
     $('body').removeClass('fh5co-offcanvas');
     var eID = '';
-    if (tag === 'nav-index'){
+    if (tag === 'nav-index') {
         eID = '#cy-page-header';
     }
-    if (tag === 'nav-purchase'){
+    if (tag === 'nav-purchase') {
         //alert(tag);
         eID = '#fh5co-feature-product';
     }
-    if (tag === 'nav-escrow'){
+    if (tag === 'nav-escrow') {
         eID = '#cy-escrow';
     }
-    if (tag ==='nav-loan'){
+    if (tag === 'nav-loan') {
         eID = '#fh5co-blog-section';
     }
-    if (tag === 'nav-design'){
+    if (tag === 'nav-design') {
         eID = '#fh5co-portfolio';
     }
-    if (tag === 'nav-investment'){
+    if (tag === 'nav-investment') {
         eID = '#cy-land';
     }
-    if (tag === 'nav-management'){
+    if (tag === 'nav-management') {
         eID = '#fh5co-services-section';
     }
-    if (tag === 'nav-about'){
-        eID ='#fh5co-contact';
+    if (tag === 'nav-about') {
+        eID = '#fh5co-contact';
     }
     $('html, body').animate({
         scrollTop: $(eID).offset().top
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,61 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Great Wall Realty - Sakura'});
+    console.log('I AM AT INDEX');
+    res.render('index', {
+        title: 'Great Wall Realty - Sakura',
+        page: 'index'
+    });
 });
+router.get('/purchase', function (req, res, next) {
+    //console.log('i am here !');
+    res.render('purchasePage', {
+        title: 'Great Wall Realty - Sakura',
+        page: 'purchase'
+
+    });
+});
+router.get('/design', function (req, res, next) {
+    res.render('designPage', {
+        title: 'Great Wall Realty - Sakura',
+        page: 'design'
+    });
+});
+router.get('/management', function (req, res, next) {
+    res.render('managementPage', {
+        title: 'Great Wall Realty - Sakura',
+        page: 'management'
+    });
+});
+router.get('/loan', function (req, res, next) {
+    res.render('loanPage', {
+        title: 'Great Wall Realty - Sakura',
+        page: 'loan'
+    });
+});
+router.get('/land', function (req, res, next) {
+    res.render('landPage', {
+        title: 'Great Wall Realty - Sakura',
+        page: 'land'
+    });
+});
+router.get('/escrow', function (req, res, next) {
+    res.render('escrowPage', {
+        title: 'Great Wall Realty - Sakura',
+        page: 'escrow'
+    });
+});
+router.get('/about', function (req, res, next) {
+    res.render('aboutPage', {
+        title: 'Great Wall Realty - Sakura',
+        page: 'about'
+    });
+});
+router.get('/privacy', function (req, res, next) {
+    res.render('privacy', {
+        title: 'Great Wall Realty - Sakura'
+    });
+});
+
 
 module.exports = router;
